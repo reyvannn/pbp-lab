d37580ef0593a70121402aaf5726953b03cff2e0
reyvannn <reyvannn@gmail.com>
2022-09-07 22:10:59 +0700

    Commit hasil awal Tutorial 1

---
+++ wishlist/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class BarangWishlist(models.Model):
+    nama_barang = models.CharField(max_length=50)
+    harga_barang = models.IntegerField()
+    deskripsi = models.TextField()
+
+data_barang_wishlist = BarangWishlist.objects.all()
+context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Kak Cinoy'
+}
